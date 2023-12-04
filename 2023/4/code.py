ab85bef28371173b8b91e81f60ed9c7dae1ce88b
Changseob Lim <22066523+changsublime@users.noreply.github.com>
2023-12-04 12:18:18 -0800

    d4p2

--- 2023/4/code.py
+++ 2023/4/code.py
@@ -10,12 +10,18 @@ if __name__ == "__main__":
         hits.append(hit.rstrip().split())
         my_nums.append(ns.rstrip().split())
 
-    for i in range(len(hits)):
+    total = len(hits)
+    num_tickets = [1] * total
+
+    for i in range(total):
         cur = 0
         for hit in hits[i]:
             if hit in my_nums[i]:
                 cur += 1
+                if i + cur < total:
+                    num_tickets[i + cur] += num_tickets[i]
         if cur:
             p1 += 2 ** (cur - 1)
     
     print(p1)
+    print(sum(num_tickets))
