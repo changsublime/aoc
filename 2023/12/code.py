1354eb4d50762711ee33dade8b3129761b518949
clim <22066523+changsublime@users.noreply.github.com>
2024-12-01 10:37:07 -0500

    working on 12

--- 2023/12/code.py
+++ 2023/12/code.py
@@ -1,16 +1,17 @@
 import re
 
-if __name__ == "__main__":
+def main():
     with open("2023/12/input.txt") as f:
         lines = f.readlines()
-    lines = """???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1""".split("\n")
-    print(lines)
+#     lines = """???.### 1,1,3
+# .??..??...?##. 1,1,3
+# ?#?#?#?#?#?#?#? 1,3,1,6
+# ????.#...#... 4,1,1
+# ????.######..#####. 1,6,5
+# ?###???????? 3,2,1""".split("\n")
+#     print(lines)
     lines = [line.rstrip().split() for line in lines]
+    # lines = [['?????#?#?', '1,1,3']]
 
     def evaluate(lines, folds=1):
         lines = [
@@ -21,16 +22,18 @@ if __name__ == "__main__":
             for line in lines
         ]
 
-        def build_possibilities(cur, remainder, check):
-            nonlocal memo
+        def build_possibilities(cur, remainder, check, memo):
             if not remainder:
                 return cur
             step = []
             for c in cur:
-                comp = tuple([len(s) for s in re.split(r'\.+', c) if s][:-1])
+                full = [len(s) for s in re.split(r'\.+', c) if s]
+                comp = tuple(full[:-1])
                 if comp in memo:
                     continue
-                elif len(comp) > 1 and comp != tuple(check[:len(comp)]):
+                elif len(full) > len(check):
+                    continue
+                elif comp and comp != tuple(check[:len(comp)]):
                     memo.add(comp)
                     continue
                 elif remainder[0] == "?":
@@ -38,22 +41,41 @@ if __name__ == "__main__":
                     step.append(c + "#")
                 else:
                     step.append(c + remainder[0])
-            return build_possibilities(step, remainder[1:], check)
+            return build_possibilities(step, remainder[1:], check, memo)
 
-        res = 0
+        res = []
         for line in lines:
             c = 0
             memo = set()
-            arrs = [int(i) for i in line[1].split(",")]
-            possibilities = build_possibilities([""], line[0], arrs)
+            arrs = [int(j) for j in line[1].split(",")]
+            possibilities = build_possibilities([""], line[0], arrs, memo)
             for p in possibilities:
                 comp = [len(s) for s in re.split(r'\.+', p) if s]
                 if comp == arrs:
                     c += 1
             if c:
+                res.append(c)
                 #print(c)
-                res += c
-        return str(res)
+        return res
 
-    print("res: " + evaluate(lines))
-    print("p2res: " + evaluate(lines, 4))
+    p1_list = evaluate(lines)
+    print(sum(p1_list))
+
+    # step = 50
+    # for i in range(0, len(lines), 50):
+    #     print(evaluate(lines[i:i+50], 5))
+
+    # p2 = 0
+    # patterns = list(zip(p1_list, evaluate(lines, 2)))
+    # for i, pattern in enumerate(patterns):
+    #     m, r = divmod(pattern[1], pattern[0])
+    #     if r:
+    #         print(i, p2)
+    #         p2 += evaluate([lines[i]], 5)[0]
+    #     else:
+    #         p2 += pattern[1] * (m ** 3)
+    # print(p2)
+
+
+if __name__ == "__main__":
+    main()
